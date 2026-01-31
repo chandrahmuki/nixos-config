653598508174311b04f4eabbe7e6c8f149133667
david <email@exemple.com>
2026-01-31 19:04:41 +0100

    adjust logo for fastfetch

--- modules/fastfetch.nix
+++ modules/fastfetch.nix
@@ -5,41 +5,47 @@
     enable = true;
     settings = {
       logo = {
-        source = "nixos_small"; # Utilise le logo NixOS compact
+        source = "nixos_small";
         padding = {
-          right = 1;
+          top = 1;
+          left = 2;
         };
       };
       display = {
-        separator = " ➜ ";
-        color = {
-          keys = "magenta";
-        };
+        separator = "  ";
       };
       modules = [
-        "title"
+        # Module "title" supprimé pour enlever david@muggy-nixos
         {
           type = "os";
           key = "OS";
+          keyColor = "blue";
         }
         {
           type = "kernel";
           key = "KR";
+          keyColor = "magenta";
         }
         {
           type = "shell";
           key = "SH";
+          keyColor = "yellow";
         }
         {
           type = "wm";
           key = "WM";
+          keyColor = "cyan";
         }
         {
           type = "uptime";
           key = "UP";
+          keyColor = "green";
         }
         "break"
-        "colors"
+        {
+          type = "colors";
+          symbol = "circle"; # Remplace les carrés par des ronds
+        }
       ];
     };
   };
