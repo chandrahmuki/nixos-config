ef67d7b2bb6514ed8ab8875b130e5f8d40088476
david <email@exemple.com>
2026-01-31 11:42:10 +0100

    fix brave clone

--- modules/brave.nix
+++ modules/brave.nix
@@ -1,13 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.brave = {
     enable = true;
-    # On force Brave à utiliser les technologies modernes (Wayland + GPU)
     commandLineArgs = [
       "--unlimited-storage"
       "--enable-features=UseOzonePlatform"
       "--ozone-platform=wayland"
     ];
   };
+
+  # Cache l'icône par défaut du système
+  home.file.".local/share/applications/brave-browser.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Brave Web Browser
+    Exec=${config.programs.brave.package}/bin/brave %U --enable-features=UseOzonePlatform --ozone-platform=wayland --unlimited-storage
+    Icon=brave-browser
+    Categories=Network;WebBrowser;
+    MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;
+    StartupWMClass=brave-browser
+    StartupNotify=true
+  '';
 }
