53de82d329d667272e1b1ea5b762d32f973f55ee
david <email@exemple.com>
2026-02-01 19:33:22 +0100

    fix(vscode): upgrade to nixfmt-rfc-style

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -3,7 +3,7 @@
 {
   # On installe les outils nécessaires au fonctionnement de l'IDE
   home.packages = with pkgs; [
-    nixfmt # Le formateur officiel
+    nixfmt-rfc-style # Le formateur officiel (RFC style)
     nil # Le "cerveau" (Language Server) pour Nix
   ];
 
@@ -44,7 +44,7 @@
         "nix.serverSettings" = {
           "nil" = {
             "formatting" = {
-              "command" = [ "${pkgs.nixfmt}/bin/nixfmt" ];
+              "command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
             };
             # Ajout pour améliorer la détection des imports
             "diagnostics" = {
