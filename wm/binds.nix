119ca8304bf97d9916c263bc2df75e95f48d6984
david <email@exemple.com>
2026-02-01 10:40:44 +0100

    adding maximizing column

--- wm/binds.nix
+++ wm/binds.nix
@@ -8,7 +8,8 @@
     ];
     "Mod+D".action = spawn "fuzzel";
     "Mod+Q".action = close-window;
-    "Mod+F".action = fullscreen-window;
+    "Mod+Shift+F".action = fullscreen-window;
+    "Mod+F".action = maximize-column;
     "Mod+T".action = spawn "foot";
     "Mod+Shift+E".action = quit { skip-confirmation = false; };
     "Mod+Shift+Slash".action = show-hotkey-overlay;
