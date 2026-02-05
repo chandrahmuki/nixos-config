5bd48e72fb74136b9e0b2a2883d55261360a4e41
david <email@exemple.com>
2026-02-05 09:31:00 +0100

    feat(yazi): configure mpv as default audio player

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -10,6 +10,34 @@
         sort_by = "modified";
         sort_dir_first = true;
       };
+      opener = {
+        audio = [
+          {
+            run = ''mpv --no-video "$@"'';
+            desc = "Play Audio";
+          }
+        ];
+      };
+      open = {
+        rules = [
+          {
+            name = "*.m4a";
+            use = "audio";
+          }
+          {
+            name = "*.mp3";
+            use = "audio";
+          }
+          {
+            name = "*.flac";
+            use = "audio";
+          }
+          {
+            name = "*.wav";
+            use = "audio";
+          }
+        ];
+      };
     };
   };
 }
