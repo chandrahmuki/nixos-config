e8ce15f660e8e3d5b71fca9c0ed577e0a9400f3d
david <email@exemple.com>
2026-02-16 09:26:21 +0100

    refactor(nvim): reorganize which-key groups and window navigation

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Basic keymaps
-map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+map("n", "<leader>qq", ":q<CR>", { desc = "Quit" })
 map("n", "<leader>w", ":w<CR>", { desc = "Save" })
 
 -- Clipboard paste
@@ -11,10 +11,10 @@ map("c", "<C-S-v>", '<C-R>+', opts)
 
 
 -- Window navigation
-map("n", "<leader>h", "<C-w>h", { desc = "Move to left window" })
-map("n", "<leader>l", "<C-w>l", { desc = "Move to right window" })
-map("n", "<leader>j", "<C-w>j", { desc = "Move to lower window" })
-map("n", "<leader>k", "<C-w>k", { desc = "Move to upper window" })
+map("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
+map("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
+map("n", "<leader>wj", "<C-w>j", { desc = "Move to lower window" })
+map("n", "<leader>wk", "<C-w>k", { desc = "Move to upper window" })
 
 
 
--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -3,6 +3,14 @@ return {
   event = "VeryLazy",
   opts = {
     preset = "modern",
+    spec = {
+      { "<leader>f", group = "file", icon = "󰈔" },
+      { "<leader>g", group = "git", icon = "󰊢" },
+      { "<leader>s", group = "search", icon = "" },
+      { "<leader>b", group = "buffer", icon = "󰓩" },
+      { "<leader>w", group = "window", icon = "󰖲" },
+      { "<leader>q", group = "quit", icon = "󰗼" },
+    },
   },
   keys = {
     {
