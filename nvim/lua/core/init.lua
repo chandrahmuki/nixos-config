072a70cc2fb8285e6097254e9c7d09b04af437fd
david <email@exemple.com>
2026-02-19 18:25:49 +0100

    fix(nvim): inline autocmds to avoid module resolution issues in Nix store

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -4,4 +4,10 @@ vim.g.maplocalleader = " "
 
 require("core.options")
 require("core.keymaps")
-require("core.autocmds")
+-- ~/.config/nvim/lua/core/autocmds.lua
+
+-- Recharger automatiquement le fichier s'il change sur le disque
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime",
+})
