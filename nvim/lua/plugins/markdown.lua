6f6b4ce7500bdb064d3d97aedc68326c635e1b1c
david <email@exemple.com>
2026-02-21 14:55:35 +0100

    feat(nvim): add render-markdown.nvim for better markdown visualization

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  ft = { "markdown", "norg", "rmd", "org" },
+  opts = {
+    heading = {
+      sign = false,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+  },
+}
