e2d30c3d580bf5568afccef9b647f2f2a045f316
david <email@exemple.com>
2026-02-25 12:58:08 +0100

    fix(nvim): make autoreload aggressive and silent

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,5 +1,5 @@
--- Recharger automatiquement le fichier s'il change sur le disque
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained", "BufEnter" }, {
+-- Recharger automatiquement et silencieusement le fichier s'il change sur le disque
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   pattern = "*",
   callback = function()
     if vim.fn.getcmdwintype() == "" then
