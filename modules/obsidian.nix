f998c8fc0b6561ae5ea6c571a385f1ec64bb049a
david <email@exemple.com>
2026-03-11 21:46:27 +0100

    feat(obsidian): add calendar community plugin to default settings

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -3,6 +3,19 @@
 {
   programs.obsidian = {
     enable = true;
-    # On peut aussi configurer des vaults ou des plugins ici si besoin
+    
+    # Configuration par défaut pour les futurs vaults
+    defaultSettings = {
+      communityPlugins = [
+        {
+          enable = true;
+          pkg = pkgs.obsidian-plugins.calendar;
+        }
+      ];
+      corePlugins = {
+        file-explorer.enable = true;
+        global-search.enable = true;
+      };
+    };
   };
 }
