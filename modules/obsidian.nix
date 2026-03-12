f521d430a9e8991ba708d7fc11466baff9cb0021
david <email@exemple.com>
2026-03-12 09:28:16 +0100

    chore(obsidian): switch to imperative plugin management (GUI based)

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -3,19 +3,5 @@
 {
   programs.obsidian = {
     enable = true;
-    
-    # Configuration par défaut pour les futurs vaults
-    defaultSettings = {
-      communityPlugins = [
-        {
-          enable = true;
-          pkg = pkgs.obsidian-plugins.calendar;
-        }
-      ];
-      corePlugins = {
-        file-explorer.enable = true;
-        global-search.enable = true;
-      };
-    };
   };
 }
