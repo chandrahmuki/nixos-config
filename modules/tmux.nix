e1ab0935b42ed696d96de77f9220c78672069f23
david <email@exemple.com>
2026-03-29 21:24:08 +0200

    feat(tmux): add modern-power tmux module

---
+++ modules/tmux.nix
@@ -0,0 +1,64 @@
+{ config, lib, pkgs, username, ... }:
+
+{
+  home-manager.users.${username} = { config, lib, ... }: {
+    programs.tmux = {
+      enable = true;
+      shortcut = "b";
+      baseIndex = 1;
+      newSession = true;
+      escapeTime = 0;
+      mouse = true;
+      keyMode = "vi";
+      terminal = "tmux-256color";
+      historyLimit = 10000;
+
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        yank
+        {
+          plugin = power-theme;
+          extraConfig = ''
+            set -g @tmux_power_theme 'moon'
+            set -g @tmux_power_user_icon ''
+            set -g @tmux_power_session_icon '󱂬'
+          '';
+        }
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            set -g @resurrect-strategy-nvim 'session'
+            set -g @resurrect-capture-pane-contents 'on'
+          '';
+        }
+        {
+          plugin = continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '5'
+          '';
+        }
+        vim-tmux-navigator
+      ];
+
+      extraConfig = ''
+        # Seamless navigation with Alt + h/j/k/l
+        bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
+        bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
+        bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
+        bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
+
+        # Intuitive splits
+        bind s split-window -v -c "#{pane_current_path}"
+        bind v split-window -h -c "#{pane_current_path}"
+
+        # Quick reload
+        bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
+
+        # Neovim detection
+        is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+            | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+      '';
+    };
+  };
+}
