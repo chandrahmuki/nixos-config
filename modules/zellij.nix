450e22695081e02a4184666cd2716cbb763cc0f2
david <email@exemple.com>
2026-04-01 23:01:52 +0200

    fix(zellij): hard revert to known working template layout with only functional fixes

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -16,14 +16,16 @@
       home.file.".config/zellij/plugins/zjstatus.wasm".source = zjstatus;
       home.file.".config/zellij/plugins/zellij-autolock.wasm".source = zellij-autolock;
 
+      programs.zellij = {
+        enable = true;
+        enableFishIntegration = false;
+      };
+
       programs.fish.shellAliases = {
         zelldev = "cd /home/${username}/nixos-config && zellij --layout dev attach -c dev";
       };
 
       programs.zellij = {
-        enable = true;
-        enableFishIntegration = false;
-
         extraConfig = ''
           keybinds {
               shared_except "locked" {
@@ -46,7 +48,7 @@
           copy_on_select = true;
           session_serialization = false;
           pane_viewport_serialization = false;
-          
+
           plugins = {
             autolock = {
               path = "file:/home/${username}/.config/zellij/plugins/zellij-autolock.wasm";
@@ -61,27 +63,30 @@
                 default_tab_template {
                     pane size=1 borderless=true {
                         plugin location="https://github.com/dj95/zjstatus/releases/download/v0.22.0/zjstatus.wasm" {
-                            format_left  "#[fg=#82a1ff,bold] 󰣆  {mode} #[fg=#3b4261,bold] {tabs}"
-                            format_right "#[fg=#82a1ff,bold]󰉖 {session} #[fg=#3b4261]| #[fg=#7aa2f7]󰊢 {command_git_branch} #[fg=#3b4261]| #[fg=#82a1ff]{datetime}"
-                            format_space "#[bg=#1e2030]"
+                            format_left  "{mode} #[fg=#89b4fa,bold]{tabs}"
+                            format_right "#[fg=#89b4fa,bold]󰉖 {session} #[fg=#424242]| #[fg=#89b4fa]󰊢 {command_git_branch} #[fg=#424242]| {datetime}"
+                            format_space ""
 
                             border_enabled  "false"
+                            border_char     "─"
+                            border_format   "#[fg=#6C7086]{char}"
+                            border_position "top"
+
                             hide_frame_for_single_pane "true"
 
-                            mode_normal  "#[fg=#82a1ff,bold]NORMAL"
-                            mode_locked  "#[fg=#f7768e,bold]LOCKED"
-                            mode_tmux    "#[fg=#ff9e64,bold]TMUX"
+                            mode_normal  "#[bg=#89b4fa,fg=#181825,bold] 󰣆 "
+                            mode_locked  "#[bg=#f38ba8,fg=#181825,bold] 󰌾 "
+                            mode_tmux    "#[bg=#ff9e64,fg=#181825,bold] 󰓩 "
 
-                            tab_normal              "#[fg=#565f89] {name} "
-                            tab_active              "#[fg=#82a1ff,bold,italic] {name} "
-                            tab_separator           "#[fg=#3b4261,bold]|"
+                            tab_normal   "#[fg=#6C7086] {name} "
+                            tab_active   "#[fg=#89b4fa,bold,italic] {name} "
 
                             command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
-                            command_git_branch_format      "#[fg=#7aa2f7] {stdout} "
+                            command_git_branch_format      "#[fg=blue] {stdout} "
                             command_git_branch_interval    "10"
                             command_git_branch_rendermode  "static"
 
-                            datetime        "#[fg=#82a1ff,bold] {format} "
+                            datetime        "#[fg=#89b4fa,bold] {format} "
                             datetime_format "%H:%M"
                             datetime_timezone "Europe/Paris"
                         }
@@ -89,13 +94,16 @@
                     children
                 }
 
-                tab name="Dev" focus=true split_direction="horizontal" {
-                    pane split_direction="vertical" size="80%" {
-                        pane command="nvim" name="MuggyVim" focus=true size="75%" {
+                tab name="Dev" focus=true {
+                    pane split_direction="vertical" {
+                        pane command="nvim" name="MuggyVim" focus=true {
                             args "."
+                            size "70%"
                             cwd "/home/${username}/nixos-config"
                         }
-                        pane command="gemini" name="AI Workspace" size="25%" {
+                        pane name="Gemini CLI" {
+                            command "gemini"
+                            size "30%"
                             cwd "/home/${username}/nixos-config"
                         }
                     }
