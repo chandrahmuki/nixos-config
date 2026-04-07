9816b110fb99e4c9ce555829c2ea5b1abc3de4f7
david <email@exemple.com>
2026-04-07 22:12:11 +0200

    fix mini.files keymaps

--- nvim/lua/plugins/minifiles.lua
+++ nvim/lua/plugins/minifiles.lua
@@ -6,9 +6,9 @@ return {
       require("mini.files").setup({
         mappings = {
           close = "q",
-          go_in = "l",
+          go_in = "<CR>",
           go_in_plus = "L",
-          go_out = "h",
+          go_out = "<BS>",
           go_out_plus = "H",
           reset_cursor = "<C-o>",
           reveal_cwd = "@",
