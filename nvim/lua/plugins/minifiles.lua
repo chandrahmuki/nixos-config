0e654360e39e4cdf165fbd7d1cb87ba4bd324607
david <amouyaljerome@gmail.com>
2026-04-08 12:28:02 +0200

    feat: mini.files Enter key opens file and closes explorer

--- nvim/lua/plugins/minifiles.lua
+++ nvim/lua/plugins/minifiles.lua
@@ -3,7 +3,19 @@ return {
     "nvim-mini/mini.nvim",
     version = false,
     config = function()
-      require("mini.files").setup({
+      local minifiles = require("mini.files")
+
+      local function go_in_and_close()
+        local entry = minifiles.get_fs_entry()
+        if entry and entry.fs_type == "file" then
+          minifiles.go_in()
+          minifiles.close()
+        else
+          minifiles.go_in()
+        end
+      end
+
+      minifiles.setup({
         mappings = {
           close = "q",
           go_in = "l",
@@ -16,6 +28,14 @@ return {
           synchronize = "=",
           toggle_preview = "P",
         },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesWindowOpen",
+        callback = function(args)
+          local buf_id = vim.api.nvim_win_get_buf(args.data.win_id)
+          vim.keymap.set("n", "<CR>", go_in_and_close, { buffer = buf_id, desc = "mini.files: open and close" })
+        end,
       })
     end,
   }
