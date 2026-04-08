a203c13d9ec25637572dcfb05d1e10f34141b97c
david <email@exemple.com>
2026-04-08 11:50:05 +0200

    refactor: consolidate gaming modules into gaming.nix

---
+++ modules/gaming.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-qt
+    lact
+    gpu-screen-recorder-gtk
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamescope = {
+    enable = true;
+  };
+
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+    settings = {
+      general = {
+        renice = 10;
+        softrealtime = "auto";
+      };
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+        amd_performance_level = "high";
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
+
+  programs.gpu-screen-recorder.enable = true;
+
+  systemd.services.lactd = {
+    description = "AMDGPU Control Daemon";
+    enable = true;
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
+}
