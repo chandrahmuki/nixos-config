a4ca02afaa4993df4f3e9653d73ff6be72559875
david <amouyaljerome@gmail.com>
2026-04-13 19:14:44 +0200

    feat(irc): configure Rizon server and auto-join #kanadi

--- modules/irc.nix
+++ modules/irc.nix
@@ -14,5 +14,18 @@
       home.packages = [
         pkgs.weechat
       ];
+
+      xdg.configFile."weechat/weechat.conf".text = ''
+        [irc]
+        channels = "#kanadi"
+        server_default = "rizon"
+
+        [irc.server.rizon]
+        addresses = "irc.rizon.net/6697"
+        tls = on
+        autojoin = "#kanadi"
+        username = "${username}"
+        realname = "${username}"
+      '';
     };
 }
