9a08b3e6af00a16d252353e94456ba2b9fd65e9a
david <amouyaljerome@gmail.com>
2026-04-13 20:03:47 +0200

    fix(irc): use Python startup script instead of declarative config files

--- modules/irc.nix
+++ modules/irc.nix
@@ -18,27 +18,35 @@
         pkgs.weechat
       ];
 
-      xdg.configFile."weechat/irc.conf" = {
+      xdg.configFile."weechat/python/autoload/setup_rizon.py" = {
         force = true;
         text = ''
-          [server_default]
-          nicks = "${username}"
-          username = "${username}"
-          realname = "${username}"
+          import weechat
 
-          [server.rizon]
-          addresses = "irc.rizon.net/6697"
-          tls = on
-          autojoin = "#kanadi"
-          autoconnect = on
-        '';
-      };
+          def setup_rizon_cb(data, buffer, args):
+              servers = weechat.infolist_get("irc_server", "", "")
+              rizon_exists = False
+              while weechat.infolist_next(servers):
+                  if weechat.infolist_string(servers, "name") == "rizon":
+                      rizon_exists = True
+                      break
+              weechat.infolist_free(servers)
 
-      xdg.configFile."weechat/xfer.conf" = {
-        force = true;
-        text = ''
-          [file]
-          download_dir = "~/Downloads/manga"
+              if not rizon_exists:
+                  weechat.command("", "/server add rizon irc.rizon.net/6697 -tls")
+                  weechat.command("", "/set irc.server.rizon.autoconnect on")
+                  weechat.command("", "/set irc.server.rizon.autojoin #kanadi")
+                  weechat.command("", "/set irc.server.rizon.nicks ${username}")
+                  weechat.command("", "/set irc.server.rizon.username ${username}")
+                  weechat.command("", "/set irc.server.rizon.realname ${username}")
+                  weechat.command("", "/set xfer.file.download_directory ~/Downloads/manga")
+                  weechat.command("", "/save")
+
+              weechat.command("", "/script remove setup_rizon")
+              return weechat.WEECHAT_RC_OK
+
+          weechat.register("setup_rizon", "${username}", "1.0", "MIT", "Setup Rizon server", "", "")
+          weechat.hook_timer(1000, 0, 1, "setup_rizon_cb", "")
         '';
       };
 
