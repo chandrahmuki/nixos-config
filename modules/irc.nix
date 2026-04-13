17b4c03d794f5007c4fad0c4fe9e586ec606cce1
david <amouyaljerome@gmail.com>
2026-04-13 19:16:46 +0200

    feat(irc): add Weechat scripts for XDCC and utilities

--- modules/irc.nix
+++ modules/irc.nix
@@ -36,5 +36,29 @@
         text = "";
         force = true;
       };
+
+      home.file."${config.home.homeDirectory}/.local/share/weechat/python/xdccq.py" = {
+        source = pkgs.fetchurl {
+          url = "https://weechat.org/files/scripts/xdccq.py";
+          sha256 = "1qshy6bdw2ypwlaylfdn2j90ib0jv90myf8is4g3qamwwq2famas";
+        };
+        executable = true;
+      };
+
+      home.file."${config.home.homeDirectory}/.local/share/weechat/python/autojoin.py" = {
+        source = pkgs.fetchurl {
+          url = "https://weechat.org/files/scripts/autojoin.py";
+          sha256 = "10y71ciiankinwi83b19fj8452vxgi1hasnwca64l0lrjgmbnrai";
+        };
+        executable = true;
+      };
+
+      home.file."${config.home.homeDirectory}/.local/share/weechat/python/autoconnect.py" = {
+        source = pkgs.fetchurl {
+          url = "https://weechat.org/files/scripts/autoconnect.py";
+          sha256 = "1va0k8304kqkv2jxkrhrfbq347pcf04fp8jwqwqz4qw2xb5zxv3m";
+        };
+        executable = true;
+      };
     };
 }
