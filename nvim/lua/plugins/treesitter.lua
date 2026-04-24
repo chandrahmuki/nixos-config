6c0c16679d6fed1ce283a27919106d4c7643726a
david <amouyaljerome@gmail.com>
2026-04-24 21:33:46 +0200

    fix(neovim): treesitter API - use nvim-treesitter instead of nvim-treesitter.configs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,22 +2,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter").setup({
       ensure_installed = {
         "nix", "lua", "rust", "markdown", "markdown_inline",
         "bash", "json", "toml", "yaml", "regex", "vim", "vimdoc",
       },
       highlight = { enable = true },
       indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
     })
   end,
 }
