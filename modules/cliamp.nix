846bd4d5ff7eec28272738a163e889b743d7f9f4
david <amouyaljerome@gmail.com>
2026-05-22 09:19:50 +0200

    feat(cliamp): update ambi playlist with new tracks

--- modules/cliamp.nix
+++ modules/cliamp.nix
@@ -95,6 +95,180 @@
       '';
     };
 
+    xdg.configFile."cliamp/playlists/ambi.toml" = {
+      text = ''
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Alien_3_92.m4a"
+        title = "Alien 3 (1992) | Ambient Soundscape"
+        artist = "A L I E N W O R L D S"
+        genre = "Music"
+        year = 2024
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Beno_NThoughts.m4a"
+        title = "Brian Eno - Night Thoughts [Stretched]"
+        artist = "Tato Schab"
+        genre = "Music"
+        year = 2025
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/BladeRunner.m4a"
+        title = "BladeRunner"
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Boards_of_Canada_55_55_B_Sides_+_Rarities_Mix.m4a"
+        title = "Boards of Canada ⬣ 55:55 (B Sides + Rarities Mix)"
+        artist = "Nomis J"
+        genre = "Music"
+        year = 2026
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/BofCanada.m4a"
+        title = "The Best Of Boards Of Canada"
+        artist = "DJ Adelaide"
+        genre = "People & Blogs"
+        year = 2025
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Above_Chiangmai_Remastered_2004.m4a"
+        title = "Above Chiangmai (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Among_Fields_Of_Crystal_Remastered_2004.m4a"
+        title = "Among Fields Of Crystal (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/An_Arc_Of_Doves_Remastered_2004.m4a"
+        title = "An Arc Of Doves (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Failing_Light_Remastered_2004.m4a"
+        title = "Failing Light (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/First_Light_Remastered_2004.m4a"
+        title = "First Light (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2018
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Not_Yet_Remembered_Remastered_2004.m4a"
+        title = "Not Yet Remembered (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Steal_Away_Remastered_2004.m4a"
+        title = "Steal Away (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/The_Chill_Air_Remastered_2004.m4a"
+        title = "The Chill Air (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/The_Plateaux_Of_Mirror_Remastered_2004.m4a"
+        title = "The Plateaux Of Mirror (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Budd/Wind_In_Lonely_Fences_Remastered_2004.m4a"
+        title = "Wind In Lonely Fences (Remastered 2004)"
+        artist = "Harold Budd, Brian Eno"
+        album = "Ambient 2: The Plateaux Of Mirror"
+        genre = "Music"
+        year = 2017
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/ET_Ambi.m4a"
+        title = "E.T. the Extra-Terrestrial | Ambient Soundscape"
+        artist = "A L I E N W O R L D S"
+        genre = "Music"
+        year = 2023
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/GTO_Study.m4a"
+        title = "Study Hard with Onizuka Sensei || 1 hour of music for studying"
+        artist = "Inuyasha Listens to music"
+        year = 2022
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Ghost_In_The_Shell_Ambient_-_Cinematic_Sci_Fi_Music_By_SpaceWave.m4a"
+        title = "Ghost In The Shell Ambient - Cinematic Sci Fi Music By SpaceWave"
+        artist = "SpaceWave - Cosmic Relaxation"
+        genre = "Music"
+        year = 2022
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Lucid_Dreams.m4a"
+        title = "Lucid_Dreams"
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Rebuild_Of_Evangelion_Sad_Calm_OST.m4a"
+        title = "Rebuild Of Evangelion Sad/Calm OST"
+        artist = "Sorikai _"
+        genre = "Music"
+        year = 2021
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Takashi_Barcelona.m4a"
+        title = "Barcelona ～ Gaudi’s Dream ～ (バルセロナ～ガウディの夢～) (1992) [Full Album]"
+        artist = "Takashi Kokubo (小久保隆)"
+        year = 2019
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/The_Goonies_1985_Ambient_Soundscape.m4a"
+        title = "The Goonies (1985) | Ambient Soundscape"
+        artist = "A L I E N W O R L D S"
+        genre = "Music"
+        year = 2024
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/TwinPeaks_Cooper.m4a"
+        title = "Twin Peaks Hotel Cafe Ambience | Morning Coffee with Agent Cooper | Calm Nostalgic 90s Soundtrack"
+        artist = "George Ander (Books and Ambience)"
+        genre = "Entertainment"
+        year = 2026
+
+        [[track]]
+        path = "${config.home.homeDirectory}/Music/Likes/Ambi/Twin_Peaks_Into_The_Night_Ambient_Soundscape.m4a"
+        title = "Twin Peaks | Into The Night | Ambient Soundscape"
+        artist = "A L I E N W O R L D S"
+        genre = "Music"
+        year = 2026
+      '';
+    };
+
     home.activation.createCliampPlaylistsDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/cliamp/playlists
     '';
