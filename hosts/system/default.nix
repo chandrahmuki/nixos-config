3363cf328841fd852e343582a02dde1b81ee590d
david <amouyaljerome@gmail.com>
2026-07-01 12:29:58 +0200

    fix: permit insecure pnpm-10.29.2 for flake update

--- hosts/system/default.nix
+++ hosts/system/default.nix
@@ -191,6 +191,11 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Allow insecure packages
+  nixpkgs.config.permittedInsecurePackages = [
+    "pnpm-10.29.2"
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
